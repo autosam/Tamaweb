736661ca6ad957e23974fd0338c8a7adf100bfe6
autosam <autosam.sm@gmail.com>
2024-02-22 23:02:14 +0330

    update service worker

--- service-worker.js
+++ service-worker.js
@@ -1,43 +1,43 @@
-self.importScripts(
-    './resources/data/CharacterDefinitions.js',
-    './resources/data/SpriteDefinitions.js',
-)
+// self.importScripts(
+//     'resources/data/CharacterDefinitions.js',
+//     'resources/data/SpriteDefinitions.js',
+// )
 
-const CACHE_NAME = 'tamaweb-v6';
+const CACHE_NAME = 'tamaweb-v7';
 const ASSETS = [
     // main
-    './index.html',
-    './src/Main.js',
-    './src/Activities.js',
-    './src/App.js',
-    './src/Drawer.js',
-    './src/Object2d.js',
-    './src/Pet.js',
-    './src/PetDefinition.js',
-    './src/Scene.js',
-    './src/Utils.js',
-    './src/ApiHelper.js',
+    'index.html',
+    'src/Main.js',
+    'src/Activities.js',
+    'src/App.js',
+    'src/Drawer.js',
+    'src/Object2d.js',
+    'src/Pet.js',
+    'src/PetDefinition.js',
+    'src/Scene.js',
+    'src/Utils.js',
+    'src/ApiHelper.js',
     // def data
-    './resources/data/CharacterDefinitions.js',
-    './resources/data/SpriteDefinitions.js',
-    './resources/data/GrowthChart.js',
+    'resources/data/CharacterDefinitions.js',
+    'resources/data/SpriteDefinitions.js',
+    'resources/data/GrowthChart.js',
     // sounds
-    './resources/sounds/angry.ogg',
-    './resources/sounds/birthday_song_01.ogg',
-    './resources/sounds/cheer.ogg',
-    './resources/sounds/eat.ogg',
-    './resources/sounds/refuse.ogg',
-    './resources/sounds/sad.ogg',
-    './resources/sounds/ui_click_01.ogg',
-    './resources/sounds/ui_click_02.ogg',
-    './resources/sounds/walk_01.ogg',
-    './resources/sounds/wedding_song_01.ogg',
-    './resources/font/PixelifySans-VariableFont_wght.ttf',
-    ...SPRITES,
-    ...PET_ADULT_CHARACTERS,
-    ...PET_TEEN_CHARACTERS,
-    ...PET_BABY_CHARACTERS,
-    ...NPC_CHARACTERS,
+    'resources/sounds/angry.ogg',
+    'resources/sounds/birthday_song_01.ogg',
+    'resources/sounds/cheer.ogg',
+    'resources/sounds/eat.ogg',
+    'resources/sounds/refuse.ogg',
+    'resources/sounds/sad.ogg',
+    'resources/sounds/ui_click_01.ogg',
+    'resources/sounds/ui_click_02.ogg',
+    'resources/sounds/walk_01.ogg',
+    'resources/sounds/wedding_song_01.ogg',
+    'resources/font/PixelifySans-VariableFont_wght.ttf',
+    // ...SPRITES,
+    // ...PET_ADULT_CHARACTERS,
+    // ...PET_TEEN_CHARACTERS,
+    // ...PET_BABY_CHARACTERS,
+    // ...NPC_CHARACTERS,
 ];
 
 self.addEventListener('install', (e) => {
