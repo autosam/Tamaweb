5c2b0e8991b620bea8d92b4b024409c9fab4be21
autosam <autosam.sm@gmail.com>
2024-02-22 22:29:53 +0330

    update cached stuff and sw version

--- resources/data/SpriteDefinitions.js
+++ resources/data/SpriteDefinitions.js
@@ -2,9 +2,16 @@ const SPRITES = [
     // backgrounds
     "resources/img/background/house/01.jpg",
     "resources/img/background/house/02.png",
+    "resources/img/background/house/03.png",
+    "resources/img/background/house/office_01.png",
     "resources/img/background/house/kitchen_01.png",
-    "resources/img/background/outside/park_02.png",
+    "resources/img/background/house/kitchen_02.png",
     "resources/img/background/house/dark_overlay.png",
+    "resources/img/background/house/wedding_01.png",
+    "resources/img/background/house/wedding_overlay.png",
+    
+    "resources/img/background/outside/park_02.png",
+    "resources/img/background/house/mall_walkway.png",
 
     // items
     "resources/img/item/foods.png",
@@ -18,4 +25,11 @@ const SPRITES = [
     "resources/img/misc/gift.png",
     "resources/img/misc/happy_icon.png",
     "resources/img/misc/laptop.png",
+    "resources/img/misc/cake_01.png",
+    "resources/img/misc/foam_01.png",
+    "resources/img/misc/foam_02.png",
+    "resources/img/misc/foam_03.png",
+    "resources/img/misc/foam_04.png",
+    "resources/img/misc/gift.png",
+    "resources/img/misc/table_01.png",
 ]
--- service-worker.js
+++ service-worker.js
@@ -1,37 +1,48 @@
-const cacheName = 'tamaweb-v2';
+self.importScripts(
+    './resources/data/CharacterDefinitions.js',
+    './resources/data/SpriteDefinitions.js',
+)
+
+const CACHE_NAME = 'tamaweb-v4';
+const ASSETS = [
+    // main
+    './index.html',
+    './src/Main.js',
+    './src/Activities.js',
+    './src/App.js',
+    './src/Drawer.js',
+    './src/Object2d.js',
+    './src/Pet.js',
+    './src/PetDefinition.js',
+    './src/Scene.js',
+    './src/Utils.js',
+    './src/ApiHelper.js',
+    // def data
+    './resources/data/CharacterDefinitions.js',
+    './resources/data/SpriteDefinitions.js',
+    './resources/data/GrowthChart.js',
+    // sounds
+    './resources/sounds/angry.ogg',
+    './resources/sounds/birthday_song_01.ogg',
+    './resources/sounds/cheer.ogg',
+    './resources/sounds/eat.ogg',
+    './resources/sounds/refuse.ogg',
+    './resources/sounds/sad.ogg',
+    './resources/sounds/ui_click_01.ogg',
+    './resources/sounds/ui_click_02.ogg',
+    './resources/sounds/walk_01.ogg',
+    './resources/sounds/wedding_song_01.ogg',
+    './resources/font/PixelifySans-VariableFont_wght.ttf',
+    ...SPRITES,
+    ...PET_ADULT_CHARACTERS,
+    ...PET_TEEN_CHARACTERS,
+    ...PET_BABY_CHARACTERS,
+    ...NPC_CHARACTERS,
+];
 
 self.addEventListener('install', (e) => {
     e.waitUntil(
-        caches.open(cacheName).then((cache) => cache.addAll([
-            // main
-            '../index.html',
-            '../src/Main.js',
-            '../src/Activities.js',
-            '../src/App.js',
-            '../src/Drawer.js',
-            '../src/Object2d.js',
-            '../src/Pet.js',
-            '../src/PetDefinition.js',
-            '../src/Scene.js',
-            '../src/Utils.js',
-            '../src/ApiHelper.js',
-            // def data
-            '../resources/data/CharacterDefinitions.js',
-            '../resources/data/SpriteDefinitions.js',
-            '../resources/data/GrowthChart.js',
-            // sounds
-            '../resources/sounds/angry.ogg',
-            '../resources/sounds/birthday_song_01.ogg',
-            '../resources/sounds/cheer.ogg',
-            '../resources/sounds/eat.ogg',
-            '../resources/sounds/refuse.ogg',
-            '../resources/sounds/sad.ogg',
-            '../resources/sounds/ui_click_01.ogg',
-            '../resources/sounds/ui_click_02.ogg',
-            '../resources/sounds/walk_01.ogg',
-            '../resources/sounds/wedding_song_01.ogg',
-            '../resources/font/PixelifySans-VariableFont_wght.ttf',
-        ])),
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)),
     );
 });
 
@@ -48,7 +59,7 @@ self.addEventListener('activate', event => {
         (async () => {
             const keys = await caches.keys();
             return keys.map(async (cache) => {
-                if (cache !== cacheName) {
+                if (cache !== CACHE_NAME) {
                     console.log('Service Worker: Removing old cache: ' + cache);
                     return await caches.delete(cache);
                 }
