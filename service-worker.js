349c82242db9e8c105136252c53877fd4de70805
saman.mohammadi <autosam.sm@gmail.com>
2024-02-27 18:13:42 +0330

    update service worker ver

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@ self.importScripts(
     'resources/data/SpriteDefinitions.js',
 )
 
-const CACHE_NAME = 'tamaweb-v9.2';
+const CACHE_NAME = 'tamaweb-v9.3';
 const ASSETS = [
     // main
     'index.html',
