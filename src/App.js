e960d9b3cac0f26019210053fc6d7d61277ec28b
autosam <autosam.sm@gmail.com>
2024-12-13 14:30:39 +0330

    change xmas event start

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ let App = {
         EVENING_TIME: [17, 20],
         NIGHT_TIME: [20, 6],
         CHRISTMAS_TIME: {
-            start: '12-13',
+            start: '12-14',
             end: '12-31',
             absDay: '12-25',
         },
