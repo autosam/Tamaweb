8ee03e33a576ff02b163954c2e76f999e5bcc6b4
saman.mohammadi <autosam.sm@gmail.com>
2025-01-13 14:16:48 +0330

    update config

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js', // Replace with your actual entry point
+  entry: './src/index.js',
   output: {
-    filename: 'bundle.js', // Output file
-    path: path.resolve(__dirname, 'dist'), // Output directory
-    // library: 'MyApp', // Assign to a global variable
-    // libraryTarget: 'window', // Attach to the global window object
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
-  mode: 'development', // Change to 'production' for production builds
-  module: {
-    rules: [
-      {
-        test: /\.js$/, // Target JavaScript files
-        exclude: /src\/libs/, // Exclude everything in the src/libs directory
-        use: {
-          loader: 'babel-loader', // Example loader (e.g., for transpiling JS)
-        },
-      },
-      // Add other loaders here if needed
-    ],
-  },
+  mode: 'development',
 };
