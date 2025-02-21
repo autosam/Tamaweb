2a4d178f6b269a0645fd019056b53945159a01c1
autosam <autosam.sm@gmail.com>
2025-02-21 15:06:12 +0330

    make pet offspring deterministic

--- src/App.js
+++ src/App.js
@@ -1242,9 +1242,10 @@ let App = {
         parentB.stats.is_player_family = true;
 
         // new pet
+        const sprite = PetDefinition.getOffspringSprite(parentA, parentB);
         newPetDefinition = new PetDefinition({
             name: getRandomName(),
-            sprite: randomFromArray(PET_BABY_CHARACTERS),
+            sprite,
         }).setStats({is_egg: true});
 
         newPetDefinition.friends = [
