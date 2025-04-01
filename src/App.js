682c6bfd47fcd7bde61729b63d97c40382bf1e58
autosam <autosam.sm@gmail.com>
2025-04-01 19:37:37 +0330

    add lifestage check to rabbitholes

--- src/App.js
+++ src/App.js
@@ -3705,6 +3705,7 @@ let App = {
                     }
                 },
                 {
+                    _disable: App.petDefinition.lifeStage == PetDefinition.LIFE_STAGE.baby,
                     name: `<span class="ellipsis">Homeworld Getaways</span> ${App.getBadge()}`,
                     onclick: () => {
                         return App.handlers.open_rabbitholes_list();
