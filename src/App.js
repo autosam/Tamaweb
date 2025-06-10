3ccc00d23bfed9925f7a88d14c57851a42a6ed8b
autosam <autosam.sm@gmail.com>
2025-06-10 21:17:41 +0330

    fix: typo

--- src/App.js
+++ src/App.js
@@ -5009,7 +5009,7 @@ const App = {
                 {
                     name: `crop match ${App.getBadge()}`,
                     onclick: () => {
-                        App.displayPopup(`Memorize the sequence of crops as the appear!`, tutorialDisplayTime, () => Activities.plantMatchingGame())
+                        App.displayPopup(`Memorize the sequence of crops as they appear!`, tutorialDisplayTime, () => Activities.plantMatchingGame())
                         return false;
                     }
                 },
