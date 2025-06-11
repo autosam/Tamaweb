52c4f5511db52baa491ff9a72104128bdf14b319
saman.mohammadi <autosam.sm@gmail.com>
2025-06-11 17:47:34 +0330

    feat: refactor input update events

--- src/App.js
+++ src/App.js
@@ -408,27 +408,22 @@ const App = {
         })
     },
     registerInputUpdates: function(){
-        document.addEventListener('mousemove', (evt) => {
+        const moveEventHandler = (evt) => {
             const rect = App.drawer.canvas.getBoundingClientRect();
-            let x = evt.clientX - rect.left, y = evt.clientY - rect.top;
-            if(x < 0) x = 0;
-            if(x > rect.width) x = rect.width;
-            if(y < 0) y = 0;
-            if(y > rect.height) y = rect.height;
-
-            App.mouse = { x: x / 2, y: y / 2 };
-        })
-        document.addEventListener('touchmove', (evt) => {
-            const rect = App.drawer.canvas.getBoundingClientRect();
-            const targetTouch = evt.targetTouches[0];
-            let x = targetTouch.clientX - rect.left, y = targetTouch.clientY - rect.top;
-            if(x < 0) x = 0;
-            if(x > rect.width) x = rect.width;
-            if(y < 0) y = 0;
-            if(y > rect.height) y = rect.height;
     
+            let x, y;
+            const target = evt.type.startsWith("touch") ? evt.targetTouches[0] : evt;
+                x = target.clientX - rect.left;
+                y = target.clientY - rect.top;
+        
+            x = Math.max(0, Math.min(x, rect.width));
+            y = Math.max(0, Math.min(y, rect.height));
+        
             App.mouse = { x: x / 2, y: y / 2 };
-        })
+        }
+
+        document.addEventListener('mousemove', moveEventHandler);
+        document.addEventListener('touchmove', moveEventHandler);
     },
     registerLoadEvents: function(){
         const initializeRenderer = () => {
