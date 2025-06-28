8e0a4ae369623a43f89d1c80e8bab2259e83c89b
autosam <autosam.sm@gmail.com>
2025-06-28 21:55:10 +0330

    feat: add show want name setting and implementation

--- src/App.js
+++ src/App.js
@@ -31,6 +31,7 @@ const App = {
         isTester: false,
         theme: false,
         shellAdditionalSize: 0,
+        showWantName: true,
     },
     constants: {
         ONE_HOUR: 1000 * 60 * 60,
@@ -2121,7 +2122,7 @@ const App = {
                     name: `current want`,
                     onclick: () => {
                         App.closeAllDisplays();
-                        App.pet.showCurrentWant();
+                        App.pet.showCurrentWant(App.settings.showWantName);
                     }
                 },
             ], null, 'Care')
@@ -2586,6 +2587,15 @@ const App = {
                                     updateUI();
 
                                     list.appendChild(content);
+                                    return true;
+                                }
+                            },
+                            {
+                                _mount: (e) => e.innerHTML = `show want name: <i>${App.settings.showWantName ? 'On' : 'Off'}</i>`,
+                                onclick: (item) => {
+                                    App.settings.showWantName = !App.settings.showWantName;
+                                    App.applySettings();
+                                    item._mount(); 
                                     return true;
                                 }
                             },
